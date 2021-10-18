9c8575610b578c6721016fe10942bbaa22f3ffb1
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2021-10-17 23:31:02 -0700

    updated server

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,34 +2,14 @@ const router = require("express").Router();
 const { notes } = require("../../db/db");
 const { createNote, getNote, deleteNote } = require("../../lib/notes");
 
-router.get("/notes", (req, res) => {
-  let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
+router.get('/notes', (req, res) => {
+    res.json(note);
 });
 
-router.get("/notes/:id", (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-router.post("/notes", (req, res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
-
-  // if any data in req.body is incorrect, send 400 error back
-  if (!validateNotes(req.body)) {
-    res.status(400).send("The Notes is not properly formatted.");
-  } else {
-    const notes = createNewNotes(req.body, notes);
+router.post('/notes', (req, res) =>{
+    req.body.id = note.length.toString();
+    const notes = createNote(req.body, note);
     res.json(notes);
-  }
-});
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,58 +5,17 @@ const fs = require("fs");
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// const { notes } = require('./db/db.json')
-
-const dbDir = path.resolve(__dirname, "db");
-const rawData = fs.readFileSync(path.resolve(dbDir, "db.json"));
-const dbNotes = JSON.parse(rawData);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 app.use(express.static('public'));
 
-// set id's
-const setIds = () => {
-  for (let i = 0; i < dbNotes.length; i++) {
-    dbNotes[i].id = i;
-  }
-}
-
-// display current notes
-app.get("/api/notes", function (req, res) {
-  setIds();
-  return res.json(dbNotes);
-});
-
-// add a new note
-app.post("/api/notes", function (req, res) {
-  const newNote = req.body;
-  dbNotes.push(newNote);
-
-  setIds();
-
-  fs.writeFileSync(path.resolve(dbDir, "db.json"), JSON.stringify(dbNotes), 'utf-8');
-  res.json(newNote);
-});
-
-// notes page
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-  });
-  
-
-// landing page
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
-  
-
+const { notes } = require('./db/db.json')
 
   // listener
   app.listen(PORT, function () {
