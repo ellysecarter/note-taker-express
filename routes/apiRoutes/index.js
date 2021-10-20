3262daa9a50240c9c8d42499858e9500b5947e10
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2021-10-19 18:45:55 -0700

    deploy

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,35 @@
-const router = require("express").Router();
-const noteRoutes = require("./noteRoutes.js");
+const fs = require('fs');
+const path = require('path');
+const router = require('express').Router();
+const notes = require('../../db/db.json');
 
-router.use(noteRoutes);
+
+
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+router.post('/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+  let postNote = req.body;
+
+  if (!postNote) {
+    res.status(400).send('The note is empty.');
+  } else {
+  notes.push(postNote);
+
+  fs.writeFile("db/db.json", JSON.stringify(notes), err => {
+      if (err) {
+          throw err;
+      } else {
+          return true;
+      }
+  });
+
+  res.json(notes);
+}
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,17 +5,20 @@ const fs = require("fs");
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+const PORT = process.env.PORT || 3006;
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 app.use(express.static('public'));
 
 const { notes } = require('./db/db.json')
+
+
 
   // listener
   app.listen(PORT, () => {
