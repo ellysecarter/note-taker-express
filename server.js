3f8527fb11ebc1db7e90d9bf673819e47eaf3c08
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2021-10-19 18:50:09 -0700

    updates

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
 app.use(express.static('public'));
 
 const { notes } = require('./db/db.json')
